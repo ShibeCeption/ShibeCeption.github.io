e17e9c661fb5f53eb88703db135f5561e531af39
ShibeCeption <80580944+ShibeCeption@users.noreply.github.com>
2023-05-02 21:57:29 +0200

    Update snake-script.js

--- snake-script.js
+++ snake-script.js
@@ -67,7 +67,7 @@
     if (e.keyCode == 8) {
       let leaveCheck = confirm("Do you wish to leave the Game?")
       leaveCheck == true
-        ? location.href = 'http://127.0.0.1:5500/arcade.html'
+        ? location.href = 'https://shibeception.github.io/index.html'
         : console.log("Leave Denied");
     }
   }
