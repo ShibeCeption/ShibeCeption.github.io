fed8a158d68008aad75bef0d80e514500693f62a
ShibeCeption <80580944+ShibeCeption@users.noreply.github.com>
2023-05-02 22:20:36 +0200

    Update snake-script.js

--- snake-script.js
+++ snake-script.js
@@ -14,7 +14,7 @@
     }
   }
 
-  let themeToggle = document.querySelector('.fa-toggle-off')
+  let themeToggle = document.querySelector('#toggleTheme')
   let themeToggle2 = document.querySelector('.toggle-text')
 
   function toggleTheme(e) {
